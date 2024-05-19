b447907be2c9006bf442f4d9a05bf074b9f0c887
Michal Kaptur <kaptur.michal@gmail.com>
2024-05-19 15:51:27 +0200

    server responded

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -8,9 +8,10 @@ int main() {
   socket.open(udp::v4());
   udp::endpoint receiver_endpoint(boost::asio::ip::make_address_v4("127.0.0.1"),
                                   4545); // FIXME don't hardcode
-  std::array<char, 1> send_buf = {{0}};
+  auto send_buf =
+      std::to_array(R"({"type":"get_pixel","position":{"x":0,"y":0}})");
   socket.send_to(boost::asio::buffer(send_buf), receiver_endpoint);
-  boost::array<char, 128> recv_buf;
+  std::array<char, 128> recv_buf;
   udp::endpoint sender_endpoint;
   size_t len =
       socket.receive_from(boost::asio::buffer(recv_buf), sender_endpoint);
